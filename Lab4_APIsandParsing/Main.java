92152353eb10a9e99d90230d54b07cec24fbf5d7
little3snake <146174173+little3snake@users.noreply.github.com>
2024-12-07 17:21:36 +0300

    Add Main.java to Lab4

---
+++ Lab4_APIsandParsing/Main.java
@@ -0,0 +1,24 @@
+import Classes.FetchData;
+import Classes.Parser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // URLs
+        String usersApi = "https://fake-json-api.mock.beeceptor.com/users";
+        String companiesApi = "https://fake-json-api.mock.beeceptor.com/companies";
+
+        System.out.println("Получаем данные пользователей:");
+        String usersResponse = FetchData.fetchDataFromApi(usersApi);
+        if (!usersResponse.isEmpty()) {
+            Parser.parseJson(usersResponse);
+        }
+
+        System.out.println("\nПолучаем данные по компаниям:");
+        String companiesResponse = FetchData.fetchDataFromApi(companiesApi);
+        if (!companiesResponse.isEmpty()) {
+            Parser.parseJson(companiesResponse);
+        }
+    }
+
+}
