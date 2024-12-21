61b860518f0750f614ce5d97cfc9e2c6660b0ff6
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2024-12-21 20:53:26 +0300

    Update Main.java

--- Lab5_ElevatorSystemThreads/Main.java
+++ Lab5_ElevatorSystemThreads/Main.java
@@ -1,21 +1,19 @@
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.*;
-
-
 public class Main {
     public static void main(String[] args) {
-        ElevatorSystem system = new ElevatorSystem(3);
-        system.start();
+        ElevatorSystem system = new ElevatorSystem(3); // Initialize the system with 3 elevators
+        system.start(); // Start the elevator system
 
-        // Run system for 30 seconds
+        // Generate requests for 30 seconds
         try {
             Thread.sleep(30000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         } finally {
-            system.stop();
+            system.stopRequestGeneration(); // Stop generating new requests
         }
+
+        // Wait until all requests are processed
+        system.waitUntilAllRequestsProcessed();
 
         System.out.println("Elevator system stopped.");
     }
